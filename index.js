873e0823f2ddc301704d9ea6851b88c8309f41d4
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-21 21:59:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,62 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord ([firstName, familyName, title, payPerHour]) {
+    const obj = {
+        firstName: `${firstName}`,
+        familyName: `${familyName}`,
+        title: `${title}`,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return obj;
+};
+
+
+function createEmployeeRecords (arrays) {
+return arrays.map(element => element = createEmployeeRecord(element))
+};
+
+
+function createTimeInEvent (employeeObject, dateHour = "YYYY-MM-DD HHMM"){
+  let obj = {
+    type : "TimeIn",
+    hour : parseInt(dateHour.slice(10)),
+    date : dateHour.slice(0,10),
+  };
+  this.timeInEvents.push(obj);
+  return employeeObject;
+};
+
+function createTimeOutEvent (employeeObject, dateHour = "YYYY-MM-DD HHMM"){
+  let obj = {
+    type : "TimeOut",
+    hour : parseInt(dateHour.slice(10)),
+    date : dateHour.slice(0,10),
+  };
+  employeeObject.timeOutEvents.push(obj);
+  return employeeObject;
+};
+
+function hoursWorkedOnDate(employeeObject, date="YYYY-MM-DD"){
+  let timeIn = employeeObject.timeInEvents.filter(checkIn => checkIn.date === date);
+  let timeOut = employeeObject.timeOutEvents.filter(checkOut => checkOut.date === date);
+  return (parseInt(timeOut[0].hour)-parseInt(timeIn[0].hour))/100;
+}
+
+function wagesEarnedOnDate (employeeObject, date="YYYY-MM-DD"){
+  let hoursWorked = hoursWorkedOnDate(employeeObject, date);
+  let rate  = employeeObject.payPerHour;
+  return (parseInt(hoursWorked))*(parseInt(rate));
+}
+
+
+function calculatePayroll(employeeArr) {
+  return employeeArr.reduce((acc, payroll) => acc + allWagesFor(payroll),0);
+}
+
+function findEmployeeByFirstName (srcArray, firstName){
+  return srcArray.find((employee) => employee.firstName === firstName);
+}
